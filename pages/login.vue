1006312f47b21b4a165f9675211ecac9cca2c4d4
mponsramells <mponsramells@gmail.com>
2024-05-22 11:15:54 +0200

    Create login page

---
+++ pages/login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div>
+      <h2>coucou je suis iki</h2>
+      <div>
+        <h3>Bravo pour ce premier pas !</h3>
+        <p>Je suis là pour t'accompagner sur ton chemin vers le bien-être.</p>
+      </div>
+      <button>C'est par ici</button>
+    </div>
+    <div>
+      <div>
+        <img src="" alt="">
+        <h2>Avant de commencer, je voudrais que tu saches que...</h2>
+        <div>
+          <div>
+            //pannel d'explication composant a créer 4 fois
+            <img src="" alt="">
+            <p></p>
+            <img src="" alt="">
+          </div>
+        </div>
+        <button>Compris, je suis prêt.e !</button>
+      </div>
+    </div>
+    <div>
+      <div>
+        <img src="" alt="">
+        <h2>Comment tu veux que je t'appelle ?</h2>
+      </div>
+      <div>
+        <p>Ecris ton nom ou surnom préféré</p>
+        <input type="text" placeholder="Léa">
+      </div>
+      <div>
+        <button>
+          <img src="" alt="">
+          <p>Précedent</p>
+        </button>
+        <button>
+          <img src="" alt="">
+          <p>Suivant</p>
+        </button>
+      </div>
+    </div>
+    <div>step4</div>
+    <div>step5</div>
+    <div>step6</div>
+    <div>step7</div>
+    <div>step8</div>
+  </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
